4a34f21f29aeb052884c852e41dc06a17fab77fa
magdy-ahmed <magdyahly2007@gmail.com>
2020-12-04 01:57:43 +0200

    second

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Biggest_offer from './biggest_offer'
+import BiggestOffer from './biggest_offer'
 import Collections from './collections';
 import Collections2 from './collections2';
 
@@ -8,7 +8,7 @@ function Content(){
     return(
         
             <div class="content-top">
-                 <Biggest_offer title1="THE BIGGEST" title2="SALE OF THE day" off="70" link="" category="Women"/>
+                 <BiggestOffer title1="THE BIGGEST" title2="SALE OF THE day" off="70" link="" category="Women"/>
           
                  <div class="banner-box-adv">
                     <Collections/>
